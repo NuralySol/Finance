5c5e864c0cf757584592da84ee8081589a51fcd7
NuralySol <soltonbekov@gmail.com>
2024-08-15 12:11:54 -0400

    commit

--- app.js
+++ app.js
@@ -264,3 +264,8 @@ app.delete('/delete-user', async (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
